72b891ec8109b16beb8cbd16a0aa9f6f5f189725
vondruska123 <vondruskaadam09@seznam.cz>
2026-02-11 11:06:58 +0100

    b

---
+++ 3.3.2/script.js
@@ -0,0 +1,16 @@
+let userName = "Martin";
+console.log(userName)
+
+userName = "Honzík";
+console.log(userName)
+
+console.log(7+4);
+console.log("7"+"4");
+console.log("A"+"B");
+console.log("7"+4);
+
+let age = 31;
+console.log(age > 20);
+
+let text = "Text"
+console.log(typeof text)
