fcc7668f051989f7b4c670bb9b7b09562e28806e
vondruska123 <vondruskaadam09@seznam.cz>
2026-03-11 10:59:49 +0100

    hg

---
+++ 3.3.3/cvičení/script.js
@@ -0,0 +1,8 @@
+let totalprice = 70
+
+if (totalprice >= 100) {
+    let discountedprice = * 0.7
+    console.log ("Cena po slevě činí" = totalprice * 0.7)
+} else {
+    console.log + ("Utrať ještě" = 100-discountedprice) + ("dolarů dostaneš slevu 30%")
+}
---
+++ 3.3.3/script.js
@@ -0,0 +1,7 @@
+let age = 20;
+
+if (age > 20) {
+    console.log("Věk je větší než 20"); 
+} else {
+    console.log("Věk je 20 nebo méně")
+}
